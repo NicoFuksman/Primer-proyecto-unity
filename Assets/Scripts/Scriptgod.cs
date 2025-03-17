640ef4bcbf85c1fd3faf6d734e8d37f0390958c5
NicoFuksman <bf0005@est.ort.edu.ar>
2025-03-17 12:14:33 -0300

    Primer proyecto Unity

---
+++ Assets/Scripts/Scriptgod.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptgod : MonoBehaviour
+{
+
+    int cantidadAlumnos;
+    float largoMesaEnM;
+    string domicilioUsuario;
+    bool aceptaCondiciones;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cantidadAlumnos = 20;
+        largoMesaEnM = 1.70f;
+        domicilioUsuario = "Libertador 5680";
+        aceptaCondiciones = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
